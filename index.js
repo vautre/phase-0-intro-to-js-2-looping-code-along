4a1dcfe84dd083a482b30bed344935513f205edc
Joyce Sheng <joycesheng96@hotmail.com>
2024-09-25 23:34:43 -0400

    saves index.js work

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+   return names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "surprise"))
+
+function countDown(number) {
+    for (let i = 10; i >= 0; i--) {
+      console.log(i);
+    }
+  }    
